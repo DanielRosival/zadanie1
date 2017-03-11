e123c1ba0845b83cfcb580f767870022f4dd5a3b
DanielRosival <daniel.rosival@gmail.com>
2017-03-11 22:36:44 +0100

    Zadanie

--- Zadanie1_Rosival/main.c
+++ Zadanie1_Rosival/main.c
@@ -7,11 +7,39 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+
+void showbits(short int x){  //Funkcia z wikipedie na kontrolu
+    int i;
+    for(i=(sizeof(short int)*8)-1; i>=0; i--)
+        (x&(1u<<i))?putchar('1'):putchar('0');
+    
+    printf("\n");
+}
+
+short int swap_endianness(short int x){
+    unsigned char aux1, aux2;
+    short int *pointer;
+    
+    pointer=&x;
+    
+    aux1= *(unsigned char *)pointer;
+    aux2= *((unsigned char *)pointer+1);
+    *(unsigned char *)pointer=aux2;
+    *((unsigned char *)pointer+1)=aux1;
+    
+    x=*pointer;
+    
+    return x;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    int x;
-    return 0;
+    short int x;
     
+    x=8;
+    showbits(x);
+    x=swap_endianness(x);
+    showbits(x);
+    
+    printf("%d\n",x);
 }
