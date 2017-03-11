2ec61b34a303d350c70b43548c5919d969dca3fd
DanielRosival <daniel.rosival@gmail.com>
2017-03-11 20:07:04 +0100

    Initial Commit

---
+++ Zadanie1_Rosival/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Zadanie1_Rosival
+//
+//  Created by Daniel Rosival on 11/03/2017.
+//  Copyright © 2017 Daniel Rosival. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
